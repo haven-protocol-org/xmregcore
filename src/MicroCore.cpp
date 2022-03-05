e853996f4b45100e1e86320cb0192d703acd7bc0
Marty <51510876+MartyHav@users.noreply.github.com>
2022-03-05 18:02:37 +0100

    adjustment signature to match haven

--- src/MicroCore.cpp
+++ src/MicroCore.cpp
@@ -186,7 +186,7 @@ MicroCore::get_hard_fork_version(uint64_t height) const
     return core_storage.get_hard_fork_version(height);
 }
 
-std::vector<uint64_t>
+std::vector<std::pair<uint64_t, uint64_t>> 
 MicroCore::get_tx_amount_output_indices(uint64_t tx_id) const
 {
     return core_storage.get_db()
